00093fa31344167397b816d07596610a47b4def7
Lautaro Montaña <joselautarom@gmail.com>
2023-11-02 10:37:36 -0300

    [FEATURE] CRUD completo

--- src/main/java/Interfaces/DAOEmpleadoImplementacion.java
+++ src/main/java/Interfaces/DAOEmpleadoImplementacion.java
@@ -4,48 +4,90 @@ import Dao.Main;
 import Personas.Empleado;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class DAOEmpleadoImplementacion implements DAOEmpleado {
 
-public class DAOEmpleadoImplementacion implements DAOEmpleado{
-    
     //Establemos una estancia de la clase main aquí
     Main main = new Main();
-    
-    public DAOEmpleadoImplementacion(){}
+
+    public DAOEmpleadoImplementacion() {
+    }
 
     @Override
     public void registrar(Empleado empleado) {
-        
+
         //Implementamos un try-catch
-        try{
-            
+        try {
+
             Connection conectar = this.main.establecerConeccion();
             PreparedStatement insertar = conectar.prepareStatement("INSERT INTO empleados (id, nombre) VALUES (?, ?)");
             insertar.setInt(1, empleado.getId());
             insertar.setString(2, empleado.getEmpleado());
             insertar.executeUpdate();
-            
-        }catch(Exception e){
+
+        } catch (Exception e) {
             System.out.println(e);
         }
-       
+
     }
 
     @Override
     public void modificar(Empleado empleado) {
-        
+
+        try {
+
+            Connection conectar = main.establecerConeccion();
+
+            PreparedStatement modificar = conectar.prepareStatement("update empleados set nombre =  ? where id = ?");
+
+            modificar.setString(1, empleado.getEmpleado());
+            modificar.setInt(2, empleado.getId());
+            modificar.executeUpdate();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 
     @Override
     public void eliminar(Empleado empleado) {
-       
+        try {
+
+            Connection conectar = main.establecerConeccion();
+
+            PreparedStatement eliminar = conectar.prepareStatement("delete from empleados where id = ?");
+
+            eliminar.setInt(1, empleado.getId());
+            eliminar.executeUpdate();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     @Override
     public void buscar(Empleado empleado) {
-        
+
+        try {
+
+            Connection conectar = main.establecerConeccion();
+
+            PreparedStatement buscar = conectar.prepareStatement("select * from empleados where id = ?");
+
+            buscar.setInt(1, empleado.getId());
+
+            ResultSet consulta = buscar.executeQuery();
+
+            if (consulta.next()) {
+                empleado.setId(Integer.parseInt(consulta.getString("id")));
+                empleado.setEmpleado(consulta.getString("nombre"));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
-    
-    
-    
+
 }
